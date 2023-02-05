16ec32b28e45c2b29b7e8f686af2b1840399f649
Jeshreen Balraj <jeshreen.balraj@itelasoft.com>
2023-02-05 22:25:32 +0530

    validation method unit tests

--- tests/unit/numberConverter.spec.js
+++ tests/unit/numberConverter.spec.js
@@ -10,12 +10,65 @@ describe('Mount Component', () => {
     expect(wrapper.exists()).toBe(true)
   })
 
-  it('das input field', () => {
+  it('has input field', () => {
     expect(wrapper.html()).toContain('Enter Binary Number')
   })
 
   it("has the expected html structure", () => {
     expect(wrapper.element).toMatchSnapshot();
   });
+})
+
+describe('getValidate function test', () => {
+  const wrapper = shallowMount(NumberConverter)
+
+  //Test if null value is passed return value is true indicating error
+  it("null check", () => {
+    let value = ""
+    let response = wrapper.vm.getValidated(value);
+    expect(response).toBe(true);
+  })
+
+  //Test if string value is passed return value is true indicating error
+  it("string check", () => {
+    let value = "test"
+    let response = wrapper.vm.getValidated(value);
+    expect(response).toBe(true);
+  })
+
+  //Test if decimal value is passed return value is true indicating error
+  it("decimal number check", () => {
+    let value = "87"
+    let response = wrapper.vm.getValidated(value);
+    expect(response).toBe(true);
+  })
+
+  //Test if negative decimal value is passed return value is true indicating error
+  it("negative number check", () => {
+    let value = "-19"
+    let response = wrapper.vm.getValidated(value);
+    expect(response).toBe(true);
+  })
+
+  //Test if negative binary value is passed return value is true indicating error
+  it("negative binary number check", () => {
+    let value = "-100"
+    let response = wrapper.vm.getValidated(value);
+    expect(response).toBe(true);
+  })
+
+  //Test if special character value is passed return value is true indicating error
+  it("special character check", () => {
+    let value = "*/"
+    let response = wrapper.vm.getValidated(value);
+    expect(response).toBe(true);
+  })
+
+  //Test if binary value is passed return value is false indicating not an error
+  it("enter correct value", () => {
+    let value = '10001'
+    let response = wrapper.vm.getValidated(value);
+    expect(response).toBe(false);
+  })
 })
 
